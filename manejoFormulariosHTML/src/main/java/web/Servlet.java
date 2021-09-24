8a1c720f1dbd288348fd388b9570e7998f4c627e
mikegomdi <mikegomez1493@gmail.com>
2021-09-24 14:31:47 -0500

    Servlet doPost Ok

--- manejoFormulariosHTML/src/main/java/web/Servlet.java
+++ manejoFormulariosHTML/src/main/java/web/Servlet.java
@@ -6,12 +6,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
 @WebServlet("/Servlet")
-public class Servlet extends HttpServlet{
+public class Servlet extends HttpServlet {
+
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-        
+
         String usuario = request.getParameter("usuario");
         String password = request.getParameter("password");
         String tecnologias[] = request.getParameterValues("tecnologia");
@@ -19,15 +20,91 @@ public class Servlet extends HttpServlet{
         String ocupacion = request.getParameter("ocupacion");
         String musica[] = request.getParameterValues("musica");
         String comentarios = request.getParameter("comentarios");
-        
+
         out.print("<html>");
         out.print("<head>");
         out.print("<title>Respuesta Servlet</title>");
         out.print("</head>");
         out.print("<body>");
         out.print("<h1>Parámetros procesados del Servlet</h1>");
+        out.print("<table border ='1'>");
+
+        out.print("<tr>");
+        out.print("<td>");
+        out.print("Usuario:");
+        out.print("</td>");
+        out.print("<td>");
+        out.print(usuario);
+        out.print("</td>");
+        out.print("<tr>");
+
+        out.print("<tr>");
+        out.print("<td>");
+        out.print("Password:");
+        out.print("</td>");
+        out.print("<td>");
+        out.print(password);
+        out.print("</td>");
+        out.print("<tr>");
+
+        out.print("<tr>");
+        out.print("<td>");
+        out.print("Tecnologías:");
+        out.print("</td>");
+        out.print("<td>");
+        for (String tecnologia : tecnologias) {
+            out.print(tecnologia);
+            out.print(" / ");
+        }
+        out.print("</td>");
+        out.print("<tr>");
+
+        out.print("<tr>");
+        out.print("<td>");
+        out.print("Genero:");
+        out.print("</td>");
+        out.print("<td>");
+        out.print(genero);
+        out.print("</td>");
+        out.print("<tr>");
+
+        out.print("<tr>");
+        out.print("<td>");
+        out.print("Ocupación:");
+        out.print("</td>");
+        out.print("<td>");
+        out.print(ocupacion);
+        out.print("</td>");
+        out.print("<tr>");
+
+        out.print("<tr>");
+        out.print("<td>");
+        out.print("Música:");
+        out.print("</td>");
+        out.print("<td>");        
+        if(musica != null){
+            for(String m:musica){
+                out.print(m);
+                out.print(" / ");
+            }
+        }else{
+            out.print("NA");
+        }        
+        out.print("</td>");
+        out.print("<tr>");
+        
+        out.print("<tr>");
+        out.print("<td>");
+        out.print("Comentarios:");
+        out.print("</td>");
+        out.print("<td>");
+        out.print(comentarios);
+        out.print("</td>");
+        out.print("<tr>");
+
+        out.print("</table>");
         out.print("</body>");
         out.print("</html>");
     }
-    
+
 }
